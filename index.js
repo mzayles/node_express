48f16564f6a7df199c3fc660426d8d9afeb218b3
Mzayles <marisouza0230@gmail.com>
2023-11-13 13:14:21 -0300

    recebendo informações no corpo da requisição.

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
-// https://localhost:3000/usuarios/20
-app.get('/usuarios/:id', (requisicao, resposta) => {
-    const id = requisicao.params.id
-    console.log(`Acessar dados do usuário ${id}`)
+app.use(express.urlencoded({
+    extended: true
+}))
 
+app.use(express.json())
+
+//GET e POST
+app.post('/cadastrar/salvar',(requisicao, resposta) => {
+   const nome = requisicao.body.nome
+   const email = requisicao.body.email
+   const senha = requisicao.body.senha
+
+   console.log(`O email do usuário é: ${email}`)
+})
+
+app.get('/cadastrar',(requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
+
+// https://localhost:3000/usuarios/20
+app.get('/usuarios/:id', (requisicao, resposta) => { 
+    const id = requisicao.params.id
+    console.log(`Acessando dados do usuário ${id}`)
     resposta.sendFile(`${caminhoBase}/usuarios.html`)
 })
 
 // https://localhost:3000/
-app.get('/', (requisicao, resposta) => {
+app.get('/',(requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
-    // resposta.send("Teste de Nodemon")
 })
 
 app.listen(3000, () => {
